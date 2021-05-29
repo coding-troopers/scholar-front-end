1592d5ca477afae0379098ed8754786ac997a1f6
Abhishek Kumar <abhi967792@gmail.com>
2021-05-29 22:48:14 +0530

    Slider component example created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import {Header} from './Components';  // import component directly from their directory 
                                       // instead of their component files 
-
+import Sliders from './Components/Sliders/Sliders';
 function App() {
   return (
     <div className="App">
       <Header/>
       <h1>Hello from App.js</h1>
+      <Sliders/>          {/* Slider by Abhishek Kumar */}
+
     </div>
   );
 }
