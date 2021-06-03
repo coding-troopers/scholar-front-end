98fe41807b038aae722b524ff38636519d800c5f
shubh4grd <shubhamburman09@gmail.com>
2021-06-03 16:32:49 +0530

    footer added

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,13 +1,48 @@
-import React, { Component } from 'react';
+import React from "react";
+import "./Footer.css";
 
-class Footer extends Component {
-    render() {
-        return (
-            <div>
-                footer component works
+function Footer() {
+    return (
+        <div className="main-footer">
+            <div className="container">
+                <div className="row">
+                    {/* Column1 */}
+                    <div className="col">
+                        <ui className="list-unstyled">
+                            <li>Scholar for Bussiness</li>
+                            <li>Teach on Scholar</li>
+                            <li>Get the app</li>
+                            <li>About us</li>
+                            <li>Conatact us</li>
+                        </ui>
+                    </div>
+                    {/* Column2 */}
+                    <div className="col">
+                        <ui className="list-unstyled">
+                            <li>Career</li>
+                            <li>Blog</li>
+                            <li>Help and Support</li>
+                            <li>Affiliate</li>
+                        </ui>
+                    </div>
+                    {/* Column3 */}
+                    <div className="col">
+                        <ui className="list-unstyled">
+                            <li>Terms</li>
+                            <li>Privacy policy</li>
+                            <li>Sitemap</li>
+                            <li>Featured cources</li>
+                        </ui>
+                    </div>
+                </div>
+                <hr />
+                <div className="row">
+                    <h2 className="col-sm">Logo</h2>
+                    <p className="col-sm">&copy;{new Date().getFullYear()} Scholar, Inc.</p>
+                </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default Footer;
