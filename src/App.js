3b6b734c029d55e97d2c2c2fdea233a5eee3dade
Abhishek Kumar <abhi967792@gmail.com>
2021-06-07 01:21:37 +0530

    Image carousel Implemented

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import './App.css';
 import {Header,Navigation} from './Components';  // import component directly from their directory 
 // instead of their component files 
-// import {Sliders} from './Components';
-// import Navigation from './Components/Body/Navigation';
-import ImageCarousel from './Components/ImageCarousel/ImageCarousel'                                       
 
-import Footer from './Components/Footer/Footer';
-import Categories from './Components/Categories/Categories';
+import {ImageCarousel,Footer,Categories} from './Components'                                       
+
 function App() {
 
   return (
     <div className="App">
       <div className="content-wrap">
       <Header/>
-      {/* <h1>Hello from App.js</h1> */}
-      {/* <Sliders/>          Slider by Abhishek Kumar */}
       <ImageCarousel/>
       <Navigation></Navigation>
       </div>
