9efc0ce4d3fdf1ccc3080d914d057fd2ad1a6fe9
Abhishek Kumar <abhi967792@gmail.com>
2021-06-06 16:39:48 +0530

    Image Carousel initiated

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import {Header} from './Components';  // import component directly from their di
                                       // instead of their component files 
 // import {Sliders} from './Components';
 import Navigation from './Components/Body/Navigation';
-                                       
+import ImageCarousel from './Components/ImageCarousel/ImageCarousel'                                       
+
 function App() {
 
   return (
@@ -11,7 +12,7 @@ function App() {
       <Header/>
       {/* <h1>Hello from App.js</h1> */}
       {/* <Sliders/>          Slider by Abhishek Kumar */}
-
+      <ImageCarousel/>
       <Navigation></Navigation>
     </div>
   );
