09c0b46a71f7be2d0b543d887e5039fba679f09b
Abhishek Kumar <abhi967792@gmail.com>
2021-06-06 23:08:43 +0530

    Created image carousel

--- src/Components/ImageCarousel/ImageCarousel.jsx
+++ src/Components/ImageCarousel/ImageCarousel.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import Carousel from 'react-material-ui-carousel'
-import { Paper, Button } from '@material-ui/core'
-
+import { Paper } from '@material-ui/core'
+import Image1 from './practice.png'
 function ImageCarousel(props)
 {
     var items = [
         {
             name: "Random Name #1",
-            description: "Probably the most random thing you have ever seen!"
+            description: "Probably the most random thing you have ever seen!",
+            url:"https://images.unsplash.com/photo-1494548162494-384bba4ab999?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZGF3bnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80"
         },
         {
             name: "Random Name #2",
-            description: "Hello World!"
+            description: "Hello World!",
+            url:Image1    
         }
     ]
 
@@ -30,10 +32,10 @@ function Item(props)
         <Paper>
             <h2>{props.item.name}</h2>
             <p>{props.item.description}</p>
-
-            <Button className="CheckButton">
+            <img src={props.item.url} alt={props.item.name}/>
+            {/* <Button className="CheckButton">
                 Check it out!
-            </Button>
+            </Button> */}
         </Paper>
     )
 }
