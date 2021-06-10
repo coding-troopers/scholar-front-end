6c186b4399668d1404be40443a7735c5ff782acf
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-06-10 22:48:51 +0530

    moving image to data/Images

---
+++ src/data/Images/category.js
@@ -0,0 +1,34 @@
+export const data = [
+    {
+        name: "design.png",
+        category: "Design"
+    },
+    {
+        name: "webdevelopment.jpeg",
+        category: "Development"
+    },
+    {
+        name: "marketing.jpg",
+        category: "Marketing"
+    },
+    {
+        name: "IT.jpg",
+        category: "IT and Software"
+    },
+    {
+        name: "personaldevelopment.jpg",
+        category: "Personal Developement"
+    },
+    {
+        name: "Business.jpg",
+        category: "Business"
+    },
+    {
+        name: "photography.jpg",
+        category: "Photography"
+    },
+    {
+        name: "MUSIC.jpg",
+        category: "Music"
+    },
+]
